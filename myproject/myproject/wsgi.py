29b93de60adf6d8c2a2a999ec056d7f49cc42bc2
jhartman <jhartman@sfu.ca>
2015-07-29 19:30:05 -0700

    Hardcode deployment vars
    hardcoded delpolyment vars in wsgi

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+sys.path.append('/home/group/final_project/myproject')
 
 application = get_wsgi_application()
