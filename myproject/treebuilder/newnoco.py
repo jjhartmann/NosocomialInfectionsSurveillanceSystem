1dd2ef95b8075d9c4633c120fcc050aece68c718
jhartman <jhartman@sfu.ca>
2015-08-05 00:49:53 -0700

    Update newnoco.py to work with Win
    Updated the newnoco to work with windows.

--- myproject/treebuilder/newnoco.py
+++ myproject/treebuilder/newnoco.py
@@ -6,25 +6,49 @@ import sys
 import pylab
 import matplotlib.pyplot as plt
 from Bio import Phylo
+import platform
 
 
 def run_phylo():
     # read in the input file, i.e. output of the blast result
-    cline = ClustalwCommandline("clustalw2", infile="fasta_temp.fa")
+    fasta_temp = os.path.abspath("./treebuilder/phylocanvas/fasta_temp.fa")
+    fasta_aln = os.path.abspath("./treebuilder/phylocanvas/fasta_temp.aln")
+    fasta_dnd = os.path.abspath("./treebuilder/phylocanvas/fasta_temp.dnd")
+    phylo_dot = os.path.abspath("./treebuilder/phylocanvas/phylo-dot.png")
+    example_xml = os.path.abspath("./treebuilder/phylocanvas/example.xml")
+    example_nwk = os.path.abspath("./treebuilder/phylocanvas/example.nwk")
+
+    cline = ClustalwCommandline("clustalw2", infile=fasta_temp)
     print(cline)
-    clustalw_exe = "/usr/bin/clustalw"
-    clustalw_cline = ClustalwCommandline(clustalw_exe, infile="fasta_temp.fa")
+
+    if platform.system() == 'Windows':
+        clustalw_exe = "C:\Program Files (x86)\ClustalW2\clustalw2.exe"
+    else:
+        clustalw_exe = "/usr/bin/clustalw"
+
+    clustalw_cline = ClustalwCommandline(clustalw_exe, infile=fasta_temp)
     assert os.path.isfile(clustalw_exe), "Clustal W executable missing"
-    stdout, stderr = clustalw_cline()
-    align = AlignIO.read("fasta_temp.aln", "clustal")
+    # stdout, stderr = clustalw_cline()
+
+    align = AlignIO.read(fasta_aln, "clustal")
+    # align = AlignIO.read("./phylocanvas/fasta_temp.aln", "clustal")
+
     print(align)
-    tree = Phylo.read("fasta_temp.dnd", "newick")
+    tree = Phylo.read(fasta_dnd, "newick")
+    # tree = Phylo.read("./phylocanvas/fasta_temp.dnd", "newick")
+
     # Phylo.draw_ascii(tree)
     tree.rooted = True
     Phylo.draw_graphviz(tree, prog="neato", node_size=0)
-    pylab.savefig('phylo-dot.png')
-    Phylo.write(tree, "example.xml", "phyloxml")
-    Phylo.write(tree, "example.nwk", "newick")
+
+    pylab.savefig(phylo_dot)
+    # pylab.savefig('./phylocanvas/phylo-dot.png')
+
+    Phylo.write(tree, example_xml, "phyloxml")
+    # Phylo.write(tree, "./phylocanvas/example.xml", "phyloxml")
+
+    Phylo.write(tree, example_nwk, "newick")
+    # Phylo.write(tree, "./phylocanvas/example.nwk", "newick")
 
     # tree = Phylo.read("example.xml", "phyloxml")
     # Phylo.draw_graphviz(tree, prog="neato", node_size=0)
