0314bf4c72008aa305bfe80b966e3ef791722ea5
hamzak <hamzak@sfu.ca>
2015-08-09 21:59:06 -0700

    Add function for REST api

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,6 +16,29 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from basic_search.models import *
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Influenza_NA
+        fields = ('completeness', 'virus_name', 'date_discovered', 'age', 'genomesegment_or_proteinname' , 'subtype', 'host', 'gender', 'sequence_length', 'genbank_accession_number')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Influenza_NA.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'nocoapi', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+
+
 urlpatterns = [
     url(r'^', include('index.urls', namespace='index')),
     url(r'^admin/', include(admin.site.urls)),
@@ -27,5 +50,6 @@ urlpatterns = [
     url(r'^(?P<username>\w+)/search/details/matrix/', include('coc.urls', namespace='coc')),
     url(r'^(?P<username>\w+)/userprofile/', include('userprofile.urls', namespace='userprofile')),
     url(r'^(?P<username>\w+)/phylo/', include('treebuilder.urls', namespace='phylo')),
+  
 ]
 
